781e8f8946e0e9c13056e13e0bf204edfa8e9c19
Maxat Kulmanov <coolmaksat@gmail.com>
2018-09-25 09:23:29 +0300

    cors

--- aberowlweb/settings.py
+++ aberowlweb/settings.py
@@ -61,11 +61,13 @@ class BaseConfiguration(Configuration):
         'widget_tweaks',
         'aberowl',
         'snowpenguin.django.recaptcha2',
+        'corsheaders',
     ]
 
     MIDDLEWARE = [
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
+        'corsheaders.middleware.CorsMiddleware',
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -241,6 +243,9 @@ class BaseConfiguration(Configuration):
         messages.WARNING: 'list-group-item-warning',
         messages.ERROR: 'list-group-item-danger',
     }
+
+    CORS_ORIGIN_ALLOW_ALL = True
+    CORS_URLS_REGEX = r'^/api/.*$'
 
 
 
